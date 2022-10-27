0db60b5e218fb47d787c3f4750ee221c11698097
Patnam <Bhavani.Patnam@Honeywell.com>
2022-10-27 16:35:16 +0530

    commented windows specific hadoop changes

--- src/main/java/com/spark/elasticsearch/crawler/CrawlerApplication.java
+++ src/main/java/com/spark/elasticsearch/crawler/CrawlerApplication.java
@@ -9,8 +9,8 @@ public class CrawlerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrawlerApplication.class, args);
 
-		System.setProperty("HADOOP_HOME", "C:\\niagara\\elk\\hadoop");
-		System.setProperty("hadoop.home.dir", "C:\\niagara\\elk\\hadoop");
+//		System.setProperty("HADOOP_HOME", "C:\\niagara\\elk\\hadoop");
+//		System.setProperty("hadoop.home.dir", "C:\\niagara\\elk\\hadoop");
 	}
 
 }
